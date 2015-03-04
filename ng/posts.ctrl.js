85aa79ae37e7b86e1bd6ea5a87ea21c533bdff42
Wendy Beck <webeck@gmail.com>
2015-03-03 17:01:20 -0800

    commit to add gulp

---
+++ ng/posts.ctrl.js
@@ -0,0 +1,19 @@
+angular.module('app')
+.controller('PostsCtrl', function ($scope, PostsSvc) {
+	$scope.addPost = function () {
+		if($scope.postBody) {
+			PostsSvc.create ({
+				username: 'Wendy',
+				body: $scope.postBody
+		}).success(function (post) { 
+			$scope.posts.unshift(post);
+			$scope.postBody = null;
+		})
+	}
+}
+
+PostsSvc.fetch()
+	.success(function(posts) {
+		$scope.posts = posts;
+	})
+})
